82e4fa3fbef1fb858272dccbdcd2f578761b8c5a
julyfun <julyfun@users.noreply.github.com>
2025-03-24 09:35:25 +0000

    deploy: d2a33f8870fdfefe74b4f3a8161973c6356eb0ff

---
+++ ch5/trait.impl/core/cmp/trait.Eq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"os/mm/page_table/struct.PTEFlags.html\" title=\"struct os::mm::page_table::PTEFlags\">PTEFlags</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"os/mm/struct.MapPermission.html\" title=\"struct os::mm::MapPermission\">MapPermission</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"os/mm/struct.PhysAddr.html\" title=\"struct os::mm::PhysAddr\">PhysAddr</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"os/mm/struct.PhysPageNum.html\" title=\"struct os::mm::PhysPageNum\">PhysPageNum</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"os/mm/struct.VirtAddr.html\" title=\"struct os::mm::VirtAddr\">VirtAddr</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"os/mm/struct.VirtPageNum.html\" title=\"struct os::mm::VirtPageNum\">VirtPageNum</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1492]}
