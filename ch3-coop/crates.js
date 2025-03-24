34a8248a1158c0deb81c7fc92aa9f9fa88bd92c3
julyfun <julyfun@users.noreply.github.com>
2025-03-24 09:34:59 +0000

    deploy: de9b3ec3900d61fa0fe1120041324b9c9f4fadbe

---
+++ ch3-coop/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["os"];
+//{"start":21,"fragment_lengths":[4]}
---
+++ ch3-coop/os/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["clear_bss","rust_main"],"macro":["print","println"],"mod":["config","console","lang_items","loader","logging","sbi","sync","syscall","task","trap"]};
