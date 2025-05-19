88c4e030e766d06e939a9dc28ccd15afd1fd8663
julyfun <julyfun@users.noreply.github.com>
2025-05-19 15:27:04 +0000

    deploy: f94fa9561aca9bf098b281261bdc5f7e6ca77463

---
+++ julyfun/trait.impl/core/marker/trait.Sync.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>",1,["os::console::Stdout"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[288]}
