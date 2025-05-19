20d498b295b46f904367142f1b9440cf075fa0d5
julyfun <julyfun@users.noreply.github.com>
2025-05-19 15:33:03 +0000

    deploy: 8bd4ee882bc44948093377dbb57f541a2cd88046

--- julyfun/search.desc/os/os-desc-0-.js
+++
@@ -1,1 +0,0 @@
-searchState.loadedDescShard("os", 0, "Returns the argument unchanged.\nCalls <code>U::from(self)</code>.")
--- julyfun/trait.impl/core/panic/unwind_safe/trait.RefUnwindSafe.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>",1,["os::console::Stdout"]]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[338]}
